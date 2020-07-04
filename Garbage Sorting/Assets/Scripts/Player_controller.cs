1d25eb7d614b4ac7fb128195ecfc08c290518cbb
YMYGABE <y2000613@icloud.com>
2020-07-04 13:37:59 +0800

    就是单纯让人物动起来了，2.5d有点新东西，得再看看

--- Garbage Sorting/Assets/Scripts/Player_controller.cs
+++ Garbage Sorting/Assets/Scripts/Player_controller.cs
@@ -1,19 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class NewBehaviourScript : MonoBehaviour
+public class Player_controller : MonoBehaviour
 {
+    public float Movespeed;  //设置人物移动速度
+
+    private Rigidbody rdb;  //获取人物刚体
+    private Animator anim;  //人物的精灵图表
     // Start is called before the first frame update
     void Start()
     {
-        
+        rdb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      Move();
+      Turn();
+    }
+
+    void Move()
+    {
+        float H = Input.GetAxis("Horizontal");
+        float V = Input.GetAxis("Vertical");
+        if (H != 0 || V != 0)
+        {
+           transform.Translate(new Vector3(H, 0, V) * Time.deltaTime * Movespeed, Space.World);
+           bool RUN = Mathf.Abs(rdb.velocity.x) > 0;
+           anim.SetBool("Run", RUN);  //动作切换，这里来改变引擎里设置的bool变量。
+        }
+        else
+        {
+           //没有移动
+        }
     }
     
+    void Turn() //用来让人物翻面
+    {
+        bool player = Mathf.Abs(rdb.velocity.x) > 0;
+        if (player)
+        {
+            if(rdb.velocity.x > 0.1f)
+            {
+                transform.localRotation = Quaternion.Euler(0,0,0);  //改变这个物体的角度，Quaternion都用来弄角度相关的东西
+            }
+            if(rdb.velocity.x < -0.1f)
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 180);  //翻转180 （x,y,z）
+                
+            }
+        }
+    }
 }
