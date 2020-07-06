c85cc4b0f9a99344fa09a0ab4ba3ec6eb09b0e0f
YMYGABE <y2000613@icloud.com>
2020-07-06 14:59:48 +0800

    实现暂停菜单

---
+++ Garbage Sorting/Assets/Script/Menu/PuseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PuseMenu : MonoBehaviour
+{
+    public static bool isPused = false; //检测游戏是否暂停状态，默认为否
+    public GameObject pauseMenuUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(isPused){
+                Resum();
+            }else{
+                Pause();
+            }
+        }
+    }
+
+    public void Resum(){
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        isPused = false;
+    }
+    public void Pause(){
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        isPused = true;
+    }
+    public void MainMenu(){
+        isPused = false;
+        SceneManager.LoadScene("Menu");
+    }
+    public void QuiteGame(){
+        Application.Quit();
+    }
+}
