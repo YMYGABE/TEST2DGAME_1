2a8fd16612d96d020acc2bef677f1701ffef022b
YMYGABE <y2000613@icloud.com>
2020-07-08 22:19:07 +0800

    正在做背包系统，明天完善

---
+++ Garbage Sorting/Assets/Script/Bag/Inventory.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Inventory",menuName = "Inventory/New Inventory")]
+public class Inventory : ScriptableObject
+{
+   List<item> Items = new List<item>();
+}
---
+++ Garbage Sorting/Assets/Script/Bag/item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+ [CreateAssetMenu(fileName = "New item",menuName = "Inventory/New item")]
+public class item : ScriptableObject
+{
+  public string ItemName;
+  public Sprite ItemImage;
+  public int ItemNum;
+}
